a57d29eb2a685e223bf262981d18c6bff872ca41
admin <admin@YD01-01>
2021-02-18 15:26:02 +0900

    variable

---
+++ javaProject/src/common/Variable1.java
@@ -0,0 +1,14 @@
+package common;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 150;
+		int n2 = 50;
+		int result = n1 + n2;
+		System.out.print("result => " + result);
+
+	}
+
+}
